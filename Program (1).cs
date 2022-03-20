9cdfb4e81bef888bc4ee02a9d2162c5156a9c841
Minhhai095 <101514121+Minhhai095@users.noreply.github.com>
2022-03-20 20:38:19 +0700

    Add files via upload

---
+++ Program (1).cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nhanvien
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Nhanvien nv = new Nhanvien();
+            Nhanvien nv1 = new Nhanvien();
+            Nhanvien nv2 = new Nhanvien();
+
+            Console.WriteLine("Nhap thong tin nhan vien 1");
+            nv1.Nhapnhanvien();
+            Console.Write("Tong tien cua nhan vien la:{0}", Nhanvien.Tinhluong(nv));
+            nv1.Innhanvien();
+
+            Console.WriteLine("Nhap thong tin nhan vien 2");
+            nv2.Nhapnhanvien();
+            Console.Write("Tong tien cua nhan vien la:{0}", Nhanvien.Tinhluong(nv));
+            nv2.Innhanvien();
+
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ Sinhvien.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SinhVien
+{ 
+
+    internal class Sinhvien
+    {
+        private string MSV;
+        private string Hoten;
+        private int Namsinh;
+        private double Diemlt;
+        private double Diemcsdl;
+        private double DTB;
+        private string Quequan;
+        public Sinhvien()
+        {
+        }
+        public Sinhvien( string hoten, int namsinh, string quequan, double diemlt, double dcsdl)
+        {
+            this.Hoten = Hoten;
+            this.Namsinh = Namsinh;
+            this.Quequan = Quequan;
+            this.Diemlt = Diemlt;
+            this.Diemcsdl = Diemcsdl;
+        }
+        public void NhapTT()
+        {
+            Console.WriteLine("Nhap ho ten:");
+            Hoten = Convert.ToString(Hoten);
+            Console.WriteLine("Nhap ma sinh vien:");
+            MSV = Convert.ToString(MSV);
+            Console.WriteLine("Nhap nam sinh:");
+            Namsinh = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Nhap diem lap trinh:");
+            Diemlt = Convert.ToDouble (Console.ReadLine());
+            Console.WriteLine("Nhap diem co so du lieu:");
+            Diemcsdl = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Nhap diem trung binh:");
+            DTB = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine(" ");
+        }
+        public void DiemTB(Sinhvien sv)
+        {
+            double DTB = (Diemlt + Diemcsdl) / 2;
+        }
+        public void XuatTT()
+        {
+            Console.WriteLine("Thong tin sinh vien la:{0}, {1}, {2},{3},{4},{5},{6}", Hoten, MSV, Namsinh, Diemlt, Diemcsdl, DTB);
+        }
+    }
+}
